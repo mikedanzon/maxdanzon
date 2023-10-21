5dd52abbf2227ae38c68e38cf6a2bf0cf76f836e
Michael Max <mikedanzon@gmail.com>
2023-10-21 19:07:13 +0700

    updated work experience

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,34 +1,41 @@
-import React from "react";
-import WorkItems from "./WorkItems";
+import React from 'react';
+import WorkItems from './WorkItems';
 
 const data = [
   {
-    year: 2020,
-    title: "hehe",
-    duration: "3 years",
-    details:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, earum ut cum, excepturi ducimus pariatur, veritatis dolore eligendi quos odit dolores recusandae voluptates. Animi voluptas corporis quod debitis qui voluptates aliquam deserunt ipsam ducimus doloremque, vero, deleniti commodi ad corrupti suscipit natus nam officiis dolor molestiae doloribus rem dolorem recusandae.",
+    yearStart: 2022,
+    yearStop: 2023,
+    title: 'Software Engineer - Canlah',
+    duration: '1 year',
+    details: `Responsible for create a backend system from start for Canlah Project, setting up Devops, helping the frontend team and managing the system.`,
+    tech: 'React JS • SASS • Go • CockroachDB • MongoDB • GCP • GKE • MySQL',
   },
   {
-    year: 2020,
-    title: "hehe",
-    duration: "3 years",
+    yearStart: 2021,
+    yearStop: 2023,
+    title: 'Software Engineer - THISCo',
+    duration: '2 years',
     details:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, earum ut cum, excepturi ducimus pariatur, veritatis dolore eligendi quos odit dolores recusandae voluptates. Animi voluptas corporis quod debitis qui voluptates aliquam deserunt ipsam ducimus doloremque, vero, deleniti commodi ad corrupti suscipit natus nam officiis dolor molestiae doloribus rem dolorem recusandae.",
+      'Responsible for Managing the automation team for Odoo, and developing 3 ways integration system for Odoo (to Retoll & Shopify). After system finished, starting to develop Quickbooks to connect to our system, also setting up mailchimp and retool.',
+    tech: 'HTML5 • CSS • Go • CockroachDB • MongoDB • GCP • GKE • MySQL • Mailchimp • Retool • Quickbooks',
   },
   {
-    year: 2020,
-    title: "hehe",
-    duration: "3 years",
+    yearStart: 2021,
+    yearStop: 2021,
+    title: 'Software Engineer - Forest AI',
+    duration: '1 year',
     details:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, earum ut cum, excepturi ducimus pariatur, veritatis dolore eligendi quos odit dolores recusandae voluptates. Animi voluptas corporis quod debitis qui voluptates aliquam deserunt ipsam ducimus doloremque, vero, deleniti commodi ad corrupti suscipit natus nam officiis dolor molestiae doloribus rem dolorem recusandae.",
+      'Responsible for creating Odoo ERP System and CRM from beginning. Developing Odoo Apps, modules and automation. Migrating Quickbooks to Odoo for Shopify.',
+    tech: 'Go • Python • MySQL',
   },
   {
-    year: 2020,
-    title: "hehe",
-    duration: "3 years",
+    yearStart: 2016,
+    yearStop: 2020,
+    title: 'Game Developer - Freelance',
+    duration: '5 years',
     details:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, earum ut cum, excepturi ducimus pariatur, veritatis dolore eligendi quos odit dolores recusandae voluptates. Animi voluptas corporis quod debitis qui voluptates aliquam deserunt ipsam ducimus doloremque, vero, deleniti commodi ad corrupti suscipit natus nam officiis dolor molestiae doloribus rem dolorem recusandae.",
+      'Reponsible for creating custom script for NPC or instances in-game events. Modify and maintenance script for clients. Setting up dedicated servers for clients.',
+    tech: 'C++ • MySQL • PHP',
   },
 ];
 
@@ -44,10 +51,12 @@ const Work = () => {
       {data.map((item, idx) => (
         <WorkItems
           key={idx}
-          year={item.year}
+          yearStart={item.yearStart}
+          yearStop={item.yearStop}
           title={item.title}
           duration={item.duration}
           details={item.details}
+          tech={item.tech}
         />
       ))}
     </div>
--- src/components/WorkItems.jsx
+++ src/components/WorkItems.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
-const WorkItems = ({ year, title, duration, details }) => {
+const WorkItems = ({ yearStart, yearStop, title, duration, details, tech }) => {
   return (
     <ol className="flex flex-col md:flex-row relative border-l border-stone-200">
       <li className="mb-10 ml-4">
         <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white" />
         <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
           <span className="inline-block px-2 py-1 font-semibold text-[--white-text-color] bg-[--main-color] rounded-md">
-            {year}
+            {yearStart} - {yearStop}
           </span>
           <span className="text-lg font-semibold text-[--main-color]">
             {title}
@@ -18,6 +18,9 @@ const WorkItems = ({ year, title, duration, details }) => {
         </p>
         <p className="my-2 text-base font-normal text-[--work-text-color]">
           {details}
+        </p>
+        <p className="my-2 text-base font-formal text-[--work-text-color]">
+          Skills - {tech}
         </p>
       </li>
     </ol>
