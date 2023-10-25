ed20b93c3c7a37b0083e420953c2310950cb2713
Michael Max <mikedanzon@gmail.com>
2023-10-25 21:38:03 +0700

    fix projects items

--- src/components/ProjectsItems.jsx
+++ src/components/ProjectsItems.jsx
@@ -4,15 +4,12 @@ const ProjectsItems = ({ image, title }) => {
   return (
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[--main-color]">
       <img src={image} alt="/" className="rounded-xl group-hover:opacity-10" />
-      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <h3 className="text-2xl font-bold text-[--white-text-color] tracking-wider text-center">
+      <div className="w-full text-center hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <h3 className="text-2xl pb-4 font-bold text-[--white-text-color] tracking-wider">
           {title}
         </h3>
-        <p className="pb-4 pt-2 text-[--white-text-color] text-center">
-          React Js
-        </p>
         <a href="/">
-          <p className="text-center p-3 rounded-lg bg-white text-[--main-color] font-bold cursor-pointer text-lg">
+          <p className="mx-[125px] sm:mx-20 text-center p-3 rounded-lg bg-white text-[--main-color] font-bold cursor-pointer text-lg">
             More Info
           </p>
         </a>
